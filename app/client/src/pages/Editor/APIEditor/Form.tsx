70ed43a1671f62e453c15fa13cbd41d33aff6da4
ohansFavour <fohanekwu@gmail.com>
2021-11-17 09:32:27 +0100

    Show evaluated value in embeddedDatasource url

--- app/client/src/pages/Editor/APIEditor/Form.tsx
+++ app/client/src/pages/Editor/APIEditor/Form.tsx
@@ -609,6 +609,7 @@ function ApiEditorForm(props: Props) {
             </BoundaryContainer>
             <DatasourceWrapper className="t--dataSourceField">
               <EmbeddedDatasourcePathField
+                dataTreePath={`${actionName}.datasourceUrl`}
                 name="actionConfiguration.path"
                 placeholder="https://mock-api.appsmith.com/users"
                 pluginId={pluginId}
